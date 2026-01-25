3cb5f961649874c543576dca0f844af9ef05d58c
Paradise <shrivastavanirmal24@gmail.com>
2026-01-25 17:40:31 +0530

    multer-file_upoas

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } from '../constants.js';
+
+cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null//upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        //file has been uploaded successfully
+        console.log('File has been uploaded on cloudinary', response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)// -> remove the locally save file as the ipload operation get failed
+        return null;
+    }
+}
+
+export { uploadOnCloudinary };
